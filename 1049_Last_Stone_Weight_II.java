89bd77854fa2f28dd6ef65769d71d0d8ac7f8d18
Gavin Lau <gavinlau0628@gmail.com>
2020-11-07 16:47:07 -0500

    Update 1049_Last_Stone_Weight_II.java

--- 1049_Last_Stone_Weight_II.java
+++ 1049_Last_Stone_Weight_II.java
@@ -1,12 +1,17 @@
 class Solution {
-    public int lastStoneWeightII(int[] stones) {
+    public int lastStoneWeightII(int[] A) {
+        // 由于限制条件：
+        // 1 <= stones.length <= 30
+        // 1 <= stones[i] <= 100
+        // 所以石头重量总和最多为 30 * 100 = 3000 / 2 = 1500+1
         boolean[] dp = new boolean[1501];
         dp[0] = true;
         int sumA = 0;
-        for (int a : stones) {
+        for (int a : A) {
             sumA += a;
+            // 一定要用--，而不是++，不然同一块石头的重量会被视作可以反复利用
             for (int i = Math.min(1500, sumA); i >= a; --i)
-                dp[i] |= dp[i - a];
+                dp[i] = dp[i] || dp[i - a];
         }
         for (int i = sumA / 2; i >= 0; --i)
             if (dp[i]) return sumA - i - i;
